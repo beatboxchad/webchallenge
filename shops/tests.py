aa4acbb28ed83fdaa8ce844d744cb62802ee8a9f
Chad Cassady <chad.cassady@gmail.com>
2018-02-26 14:11:33 -0600

    comments added to tests.py in prep for TDD workflow

--- shops/tests.py
+++ shops/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# As an Administrator, I can promote other users to administrator
+
+
+# As an Administrator, I can load shops to the database, from an uploaded json file (shops.json provided).
+
+# As a User (or Administrator), I can sign up using my email & password
+
+# As a User (or Administrator), I can sign in using my email & password
+
+# As a User (or Administrator), I can display the list of shops
+# As a User, I can like a shop, so it can be added to my preferred shops
+# Acceptance criteria: liked shops shouldn’t be displayed on the main page
+# As a User, I can display the list of preferred shops
+# As a User, I can remove a shop from my preferred shops list
