2e59c392cc87025cb583811d5b718e0b729fd80d
Chad Cassady <chad.cassady@gmail.com>
2018-03-04 08:50:48 -0600

    added all static files; beginning work on serverside auth

--- shops/serializers.py
+++ shops/serializers.py
@@ -1,6 +1,6 @@
-from rest_framework import serializers
-from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from rest_framework import serializers
 from shops.models import Shop
 
 User = get_user_model()
@@ -8,7 +8,7 @@ User = get_user_model()
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups', 'shops')
+        fields = ('url', 'username', 'email', 'shops', 'is_superuser')
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
